66a5f4ccc589c0a57193dec9d8d4e83b81364110
el0xren <el0xren@gmail.com>
2021-07-19 15:56:56 +0300

    handlers: Fixed handlers

--- tg_bot/modules/helper_funcs/handlers.py
+++ tg_bot/modules/helper_funcs/handlers.py
@@ -2,13 +2,48 @@ import telegram.ext as tg
 from telegram import Update
 from tg_bot import ALLOW_EXCL
 
-CMD_STARTERS = ('/', '!')
+if ALLOW_EXCL:
+    CMD_STARTERS = ("/', '!")
+else:
+    CMD_STARTERS = ("/",)
+
 
 class CustomCommandHandler(tg.CommandHandler):
-    def __init__(self, command, callback, **kwargs):
+    def __init__(self, command, callback, run_async=True, **kwargs):
         if "admin_ok" in kwargs:
             del kwargs["admin_ok"]
         super().__init__(command, callback, **kwargs)
+
+    def check_update(self, update):
+        if isinstance(update, Update) and update.effective_message:
+            message = update.effective_message
+            try:
+                user_id = update.effective_user.id
+            except:
+                user_id = None
+
+            if message.text and len(message.text) > 1:
+                fst_word = message.text.split(None, 1)[0]
+                if len(fst_word) > 1 and any(
+                    fst_word.startswith(start) for start in CMD_STARTERS
+                ):
+
+                    args = message.text.split()[1:]
+                    command = fst_word[1:].split("@")
+                    command.append(message.bot.username)
+                    if user_id == 1087968824:
+                        user_id = update.effective_chat.id
+                    if not (
+                        command[0].lower() in self.command
+                        and command[1].lower() == message.bot.username.lower()
+                    ):
+                        return None
+                    filter_result = self.filters(update)
+                    if filter_result:
+                        return args, filter_result
+                    else:
+                        return False
+
 
 class CustomRegexHandler(tg.MessageHandler):
     def __init__(self, pattern, callback, friendly="", **kwargs):
