e1b8f2cd67261f94effbe77b85ede0793f89455e
el0xren <el0xren@gmail.com>
2021-07-19 16:40:42 +0300

    admin: Minor formating

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -274,15 +274,15 @@ def info(update: Update, context: CallbackContext):
         text += "\n\nThis person is my owner - I would never do anything against them!"
     else:
         if user.id in SUDO_USERS:
-            text += "\nThis person is one of my sudo users! " \
+            text += "\n\nThis person is one of my sudo users! " \
                     "Nearly as powerful as my owner - so watch it."
         else:
             if user.id in SUPPORT_USERS:
-                text += "\nThis person is one of my support users! " \
+                text += "\n\nThis person is one of my support users! " \
                         "Not quite a sudo user, but can still gban you off the map."
 
             if user.id in WHITELIST_USERS:
-                text += "\nThis person has been whitelisted! " \
+                text += "\n\nThis person has been whitelisted! " \
                         "That means I'm not allowed to ban/kick them."
 
     for mod in USER_INFO:
