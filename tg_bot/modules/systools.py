57b6c8a2f221f98445875008bd7861e145d35836
Sayan Biswas <sayan@pokurt.me>
2021-09-27 22:09:49 +0530

    Dev: few changes to /leave allow sending messages before leaving chat, show confirmation message on not passing args.
    Signed-off-by: Sayan Biswas <sayan@pokurt.me>
    Signed-ff-by: el0xren <el0xren@gmail.com>

--- tg_bot/modules/systools.py
+++ tg_bot/modules/systools.py
@@ -1,9 +1,13 @@
+import requests
+import re
+
 from subprocess import Popen, PIPE
 
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, Filters
+from telegram.ext.callbackqueryhandler import CallbackQueryHandler
 
-from tg_bot import dispatcher, CallbackContext, OWNER_ID
+from tg_bot import dispatcher, CallbackContext, OWNER_ID, SUDO_USERS
 
 
 def shell(update: Update, context: CallbackContext):
@@ -30,13 +34,32 @@ def leave(update: Update, context: CallbackContext):
     args = context.args
     if args:
         chat_id = str(args[0])
-        del args[0]
+        leave_msg = " ".join(args[1:])
         try:
+            context.bot.send_message(chat_id, leave_msg)
             bot.leave_chat(int(chat_id))
-        except telegram.TelegramError:
-            update.effective_message.reply_text("Couldn't leave group.")
+            update.effective_message.reply_text("Left chat.")
+        except TelegramError:
+            update.effective_message.reply_text("Failed to leave chat for some reason.")
     else:
-        update.effective_message.reply_text("Send a valid chat id.")
+        chat = update.effective_chat
+        kb = [[
+            InlineKeyboardButton(text="I am sure of this action.", callback_data="leavechat_cb_({})".format(chat.id))
+        ]]
+        update.effective_message.reply_text("I'm going to leave {}, press the button below to confirm".format(chat.title), reply_markup=InlineKeyboardMarkup(kb))
+
+
+def leave_cb(update: Update, context: CallbackContext):
+    bot = context.bot
+    callback = update.callback_query
+    if callback.from_user.id not in SUDO_USERS:
+        callback.answer(text="This isn't for you", show_alert=True)
+        return
+
+    match = re.match(r"leavechat_cb_\((.+?)\)", callback.data)
+    chat = int(match.group(1))
+    bot.leave_chat(chat_id=chat)
+    callback.answer(text="Left chat")
 
 
 def get_bot_ip(update: Update, context: CallbackContext):
@@ -49,10 +72,12 @@ def get_bot_ip(update: Update, context: CallbackContext):
 
 SHELL_HANDLER = CommandHandler(["sh", "shell"], shell, filters=Filters.user(OWNER_ID), run_async=True)
 LOG_HANDLER = CommandHandler("logs", logs, filters=Filters.user(OWNER_ID), run_async=True)
-LEAVE_HANDLER = CommandHandler("leave", leave, filters=Filters.user(OWNER_ID), run_async=True)
+LEAVE_HANDLER = CommandHandler("leave", leave, run_async=True)
+LEAVE_CALLBACK = CallbackQueryHandler(leave_cb, pattern=r"leavechat_cb_", run_async=True)
 IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.user(OWNER_ID), run_async=True)
 
 dispatcher.add_handler(SHELL_HANDLER)
 dispatcher.add_handler(LOG_HANDLER)
 dispatcher.add_handler(LEAVE_HANDLER)
+dispatcher.add_handler(LEAVE_CALLBACK)
 dispatcher.add_handler(IP_HANDLER)
